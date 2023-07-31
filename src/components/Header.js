02d88da880ee4e515fd2a4540069fc124a0786c8
honeymirara <honeymirara@gmail.com>
2023-08-01 01:34:27 +0300

    start-app-footer-header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function header() {
+    return (
+        <header>
+            <div>
+                <span className='logo'> House staff </span>
+            </div>
+            <div className='presentation'></div>
+        </header>
+    )
+}
+
+
